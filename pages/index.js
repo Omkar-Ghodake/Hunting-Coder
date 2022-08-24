23e86e3889ec7a35454ae8b055331e63eb6af8a2
Omkar-Ghodake <omkar.a.g.78@gmail.com>
2022-08-24 19:46:44 +0530

    Added Navbar

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <nav className={ styles.navbar }>
+        <ul className={ styles.navList }>
+          <a href="/"><li className={ styles.navItem }>Home</li></a>
+          <a href="/"><li className={ styles.navItem }>Blogs</li></a>
+          <a href="/"><li className={ styles.navItem }>About</li></a>
+          <a href="/"><li className={ styles.navItem }>Contact</li></a>
+        </ul>
+      </nav>
+
       <main className={ styles.main }>
         <h1 className={ styles.title }>
           Hunting Coder
@@ -20,48 +29,32 @@ export default function Home() {
           A blog for Hunting Coders by a Hunting Coders
         </p>
 
-        <div className={ styles.grid }>
-          <a href="https://nextjs.org/docs" className={ styles.card }>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className="blogs">
+          <h2>Popular Blogs</h2>
+          <div className="blog-item">
+            <h3>How to learn JavaScript in 2022?</h3>
+            <p>Javascript is the language used to design logic for the web.</p>
+          </div>
 
-          <a href="https://nextjs.org/learn" className={ styles.card }>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className="blog-item">
+            <h3>How to learn JavaScript in 2022?</h3>
+            <p>Javascript is the language used to design logic for the web.</p>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={ styles.card }
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <div className="blog-item">
+            <h3>How to learn JavaScript in 2022?</h3>
+            <p>Javascript is the language used to design logic for the web.</p>
+          </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={ styles.card }
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="blog-item">
+            <h3>How to learn JavaScript in 2022?</h3>
+            <p>Javascript is the language used to design logic for the web.</p>
+          </div>
         </div>
       </main>
 
       <footer className={ styles.footer }>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{ ' ' }
-          <span className={ styles.logo }>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={ 72 } height={ 16 } />
-          </span>
-        </a>
+
       </footer>
     </div>
   )
